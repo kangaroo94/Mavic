da00cac245132f3119088d638fc5d0c5b5d78787
binkovskyi94 <guidekinoguidekino@gmail.com>
2020-08-04 14:25:24 +0300

    added animation scroll (fullpage.js)

--- gulpfile.js
+++ gulpfile.js
@@ -127,6 +127,7 @@ gulp.task("style", function () {
 			"node_modules/slick-carousel/slick/slick.css",
 			"node_modules/slick-carousel/slick/slick-theme.css",
 			"node_modules/normalize.css/normalize.css",
+			"node_modules/fullpage.js/dist/fullpage.css",
 		])
 		.pipe(sourcemaps.init())
 		.pipe(concat("libs.min.css")) //склеиваем их в один файл с указанным именем
@@ -143,6 +144,7 @@ gulp.task("script", function () {
 			//тут подключаем разные js в общую библиотеку. Отключите то, что вам не нужно.
 			"node_modules/jquery/dist/jquery.js",
 			"node_modules/slick-carousel/slick/slick.js",
+			"node_modules/fullpage.js/dist/fullpage.js",
 			
 		])
 		.pipe(size())
